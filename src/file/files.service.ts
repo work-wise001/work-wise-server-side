d73a695d2749445b27183831338d7bfdc65e9dab
Michstery <macaulaymich@gmail.com>
2024-07-17 17:09:27 +0100

    feat/ added the update photo functionality

---
+++ src/file/files.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+// import { InjectModel } from "@nestjs/mongoose";
+// import { Model } from "mongoose";
+// import { Product } from "./products.model";
+// import { v4 as uuidv4 } from "uuid";
+import fs from 'fs';
+import { promisify } from 'util';
+import cloudinary from '../utils/cloudinary.config';
+
+@Injectable()
+export class FileService {
+  //private products: Product[] = [];
+
+//   constructor(
+//     //@InjectModel("Product") private readonly productModel: Model<Product>
+//   ) {}
+
+    public async unlinkFile(path: string) {
+        await promisify(fs.unlink)(path);
+    }
+
+    public async deleteFile(publicId: any) {
+        try{
+          const deletedFile = await cloudinary.uploader.destroy(publicId);
+          if(!deletedFile){
+            return 'Photo Deletion Failed'
+          }
+          return 'File deleted Successfully'
+        } catch (error) {
+          throw new Error(`Error deleting file: ${error}`);
+          
+        }
+    
+    }
+
+    public async uploadSinglePhoto(file: Express.Multer.File, dir = 'uploads') {
+        const { fieldname, size, path } = file;
+    
+        if (size > 5 * 1024 * 1024) {
+          await this.unlinkFile(path);
+          throw new Error(`file upload failed, file size is too large, check that the file size is less than 5mb. file: ${fieldname}`);
+        }
+        const image = await cloudinary.uploader.upload(path);
+        return image;
+      }
+
+}

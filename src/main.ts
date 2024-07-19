8ee2ca4a6a2fdf9ac1a44c046bbd6a5829a9ea20
Michstery <macaulaymich@gmail.com>
2024-07-19 14:09:26 +0100

    feat/swagger setup

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,31 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as swaggerUi from 'swagger-ui-express';
-import { writeFileSync } from 'fs';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // swagger set up
-  const config = new DocumentBuilder()
-  .setTitle('Work Wise API Server')
-  .setDescription('The Work Wise API description')
-  .setVersion('1.0')
-  .addTag('api')
-  .addServer('https://work-wise-server-side.onrender.com/') // Online server
-  .addServer('http://localhost:3000') // Local server
-  .build();
-const document = SwaggerModule.createDocument(app, config);
-  // Save the JSON file to disk
-  writeFileSync('./swagger.json', JSON.stringify(document, null, 2));
+//   // swagger set up
+//   const config = new DocumentBuilder()
+//   .setTitle('Work Wise API Server')
+//   .setDescription('The Work Wise API description')
+//   .setVersion('1.0')
+//   .addTag('api')
+//   .addServer('https://work-wise-server-side.onrender.com/') // Online server
+//   .addServer('http://localhost:3000') // Local server
+//   .build();
+// const document = SwaggerModule.createDocument(app, config);
+//   // Save the JSON file to disk
+//   writeFileSync('./swagger.json', JSON.stringify(document, null, 2));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(document));
+    // Read the swagger.json file
+    const swaggerDocument = JSON.parse(readFileSync(join(__dirname, '..', 'swagger.json'), 'utf8'));
+
+    // Serve swagger.json using swagger-ui-express
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(document));
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
